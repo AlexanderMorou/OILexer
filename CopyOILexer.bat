73de539ce1dcab361989017330832fe54b4895af
AlexanderMorou_cp <SND\AlexanderMorou_cp>
2015-07-12 04:52:14 +0000

    Checking-in missing files

---
+++ CopyOILexer.bat
@@ -0,0 +1,8 @@
+@echo off
+Copy %1Samples\OILexer\OILexer.oilexer %1Samples\ToySharp\Language\Language.oilexer
+Copy %1Samples\OILexer\Preprocessor.oilexer %1Samples\ToySharp\Language\LanguagePreprocessor.oilexer
+Copy %1Samples\OILexer\Errors.oilexer %1Samples\ToySharp\Language\Errors.oilexer
+Copy %1Samples\OILexer\GrammarDefinitionFile.oilexer %1Samples\ToySharp\Language\GrammarDefinitionFile.oilexer
+Copy %1Samples\OILexer\Rules.oilexer %1Samples\ToySharp\Language\Rules.oilexer
+Copy %1Samples\OILexer\Templates.oilexer %1Samples\ToySharp\Language\Templates.oilexer
+Copy %1Samples\OILexer\Tokens.oilexer %1Samples\ToySharp\Language\Tokens.oilexer
